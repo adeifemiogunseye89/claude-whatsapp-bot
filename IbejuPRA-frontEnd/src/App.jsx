5a39950f6640be2ae34cf29b86fbb5cde96754aa
Adebanjo ogunseye <adebanjo.ogunse@gmai.com>
2026-08-09 23:39:17 +0100

    Initial ibeju lekki  chatbot deployment commit

---
+++ IbejuPRA-frontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import IbejuLekkiAssistant from './IbejuLekkiAssistant'; // Fixed path
+
+function App() {
+  return (
+    <div>
+      <IbejuLekkiAssistant />
+    </div>
+  );
+}
+
+export default App;
+
